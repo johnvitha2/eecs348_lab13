df151ebb2cfddf218ce502c3cf513ae959fc7219
John Vitha <johnvitha02@gmail.com>
2025-11-30 15:40:47 -0600

    Add test function

--- algorithm1.cpp
+++ algorithm1.cpp
@@ -1,7 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 bool fail_lecture(const std::vector<int>& attendance_records) {
     int absent_count = 0;
     for (int i = 1; i < attendance_records.size(); ++i) {
         absent_count += (attendance_records[i] == 0);
     }
     return absent_count >= 3;
+}
+
+void run_test(const vector<int>& attendance_records, bool expected, const string& question) {
+    bool actual = fail_lecture(attendance_records);
+    cout << question;
+    cout << "Test case: ";
+    for (int attendance : attendance_records) cout << attendance << " ";
+    cout << "Expected Output: " << expected << endl;
+    cout << "Actual Output:   " << actual << endl;
 }
