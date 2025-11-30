8f8e178d7f39fb056705fe6b360ee4cda90610cc
John Vitha <johnvitha02@gmail.com>
2025-11-30 15:42:33 -0600

    Add tf function to make output more readable

--- algorithm1.cpp
+++ algorithm1.cpp
@@ -10,11 +10,15 @@ bool fail_lecture(const std::vector<int>& attendance_records) {
     return absent_count >= 3;
 }
 
+string tf(bool output) {
+    return output ? "True" : "False";
+}
+
 void run_test(const vector<int>& attendance_records, bool expected, const string& question) {
     bool actual = fail_lecture(attendance_records);
     cout << question;
     cout << "Test case: ";
     for (int attendance : attendance_records) cout << attendance << " ";
-    cout << "Expected Output: " << expected << endl;
-    cout << "Actual Output:   " << actual << endl;
+    cout << "Expected Output: " << tf(expected) << endl;
+    cout << "Actual Output:   " << tf(actual) << endl << endl;
 }
